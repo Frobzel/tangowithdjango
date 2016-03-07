3c7399347f77fa0c5e7fb51ba2a9e59d6a43e2e5
Frobzel <bjonynas@gmail.com>
2016-03-07 15:13:42 +0000

    chapter 11 complete

--- tangoWithDjangoProject/rango/views.py
+++ tangoWithDjangoProject/rango/views.py
@@ -5,12 +5,36 @@ from django.http import HttpResponseRedirect, HttpResponse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
 from rango.forms import UserForm, UserProfileForm
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+    
     context_dict = {'categories':category_list, 'pages':page_list}
-    return render(request, 'rango/index.html', context_dict)
+
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            visits = visits + 1
+            reset_last_visit_time = True
+    else:
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+    
+    response = render(request, 'rango/index.html', context_dict)
+    return response
 
 def about(request):
     context_dict = {'anothermessage': "I mean look at this image of the highest quality: "}
@@ -72,7 +96,7 @@ def add_page(request, category_name_slug):
 
     return render(request, 'rango/add_page.html', context_dict)
 
-def register(request):
+def register(request):    
     registered = False
 
     if request.method == 'POST':
